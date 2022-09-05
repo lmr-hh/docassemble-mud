1c86273e7fbd84340ccb6ea051462cce0cb8f7e7
Kim Wittenburg <codello@wittenburg.kim>
2022-09-05 16:32:59 +0200

    Add replacement workshop

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude,
 
 setup(
     name='docassemble.mud',
-    version='1.0.0',
+    version='1.0.1',
     description=__doc__,
     long_description=long_description(),
     long_description_content_type='text/markdown',
